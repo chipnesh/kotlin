3c880fa05df46f38f83c4fdaf74b7e1daf83189c
Artem Kobzar <artem.kobzar@jetbrains.com>
2021-10-07 16:43:13 +0300

    test: rework Kt-47096 test for JVM.

--- js/js.tests/tests-gen/org/jetbrains/kotlin/js/test/semantics/BoxJsTestGenerated.java
+++ js/js.tests/tests-gen/org/jetbrains/kotlin/js/test/semantics/BoxJsTestGenerated.java
@@ -8018,10 +8018,5 @@ public class BoxJsTestGenerated extends AbstractBoxJsTest {
         public void testJsExternalVarargSuspend() throws Exception {
             runTest("js/js.translator/testData/box/vararg/jsExternalVarargSuspend.kt");
         }
-
-        @TestMetadata("useSuspendFunResultAsVararg.kt")
-        public void testUseSuspendFunResultAsVararg() throws Exception {
-            runTest("js/js.translator/testData/box/vararg/useSuspendFunResultAsVararg.kt");
-        }
     }
 }
